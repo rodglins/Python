3ca4f03902ce8d0f6d0bdd9c644c0c0cee205488
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-04 19:10:07 -0300

    Testes com Funções Python

---
+++ fundamentos/Funcoes/ModuloFuncoes.py
@@ -0,0 +1,22 @@
+from Listas_Funcoes.Funcoes import *
+
+minhaLista=[]
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista, 20)
+
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+
+print("Resumindo")
+resumirValores(minhaLista)
+
+
+
