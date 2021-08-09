489b8dbd537f1f52a13986a827ffe5e54ab8671c
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-09 14:58:56 -0300

    Threads

--- seguranca/threads.py
+++ seguranca/threads.py
@@ -2,24 +2,26 @@
 
 #importando a classe thread:
 from threading import Thread
+import time
 
-# função 1
-def carro1(velocidade):
+# função carros:
+def carro(velocidade, piloto):
     trajeto = 0
     while trajeto <= 100:
-        print('Carro1: ', trajeto)
+        print('Carro: ', piloto, trajeto)
         trajeto += velocidade
+        time.sleep(0.5)
 
-# função 2
-def carro2(velocidade):
-    trajeto = 0
-    while trajeto <= 100:
-        print('Carro2: ', trajeto)
-        trajeto += velocidade
+# # função 2
+# def carro2(velocidade):
+#     trajeto = 0
+#     while trajeto <= 100:
+#         print('Carro2: ', trajeto)
+#         trajeto += velocidade
 
-# chamando a função com velocidade 10
-t_carro1 = Thread(target=carro1, args=[1])
-t_carro2 = Thread(target=carro1, args=[1.5])
+# chamando a função
+t_carro1 = Thread(target=carro, args=[1, 'Rodrigo'])
+t_carro2 = Thread(target=carro, args=[2, 'Python'])
 
 #start carros
 t_carro1.start()
