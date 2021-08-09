6c4208ee6db75b69faf9a8eb1dbae3da306c5d7c
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-09 16:42:58 -0300

    Gerador de wordlists

--- seguranca/gerador_wordlist.py
+++ seguranca/gerador_wordlist.py
@@ -2,8 +2,11 @@
 # biblioteca de iterações
 import itertools
 
+# inserir palavra para gerar wordlist
+string = input('String a ser permutada: ')
+
 # variável que receberá o itertools:
-resultado = itertools.permutations('abcdefmopr', 4)
+resultado = itertools.permutations(string, len(string))
 
 # gera as combinações possiveis de forma aleatória
 for i in resultado:
