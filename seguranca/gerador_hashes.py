b74c919950eac102e4e706cc5f479ad9aa01e0f0
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-09 16:22:58 -0300

    Gerador de hashes

--- seguranca/gerador_hashes.py
+++ seguranca/gerador_hashes.py
@@ -1,7 +1,30 @@
 # biblioteca hashlib
 import hashlib
 
-# variavel resultado que recebe hashlib
-resultado = hashlib.md5(b'Python Security')
+# Inserindo o texto
+string = input('Digite o texto para gerar a hash: ')
 
-print('O hash md5 da sring é: ', resultado.hexdigest())
+# escolhendo o codificador:
+menu = int(input(''' #### MENU - ESCOLHA O TIPO DE HASH ####
+                1) MD5
+                2) SHA1
+                3) SHA256
+                4) SHA512
+                Digite o número do hash a gerar: '''))
+
+# Informações do menu
+if menu == 1:
+    # variavel resultado que recebe hashlib
+    resultado = hashlib.md5(string.encode('utf-8'))
+    print('O hash MD5 da sring é: ', string, 'é: ', resultado.hexdigest())
+elif menu == 2:
+    resultado = hashlib.sha1(string.encode('utf-8'))
+    print('O hash SHA1 da sring é: ', string, 'é: ', resultado.hexdigest())
+elif menu == 3:
+    resultado = hashlib.sha256(string.encode('utf-8'))
+    print('O hash SHA256 da sring é: ', string, 'é: ', resultado.hexdigest())
+elif menu == 4:
+    resultado = hashlib.sha512(string.encode('utf-8'))
+    print('O hash SHA512 da sring é: ', string, 'é: ', resultado.hexdigest())
+else:
+    print('Algo está errado, tente novamente')
