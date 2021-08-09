7fbf787af4f9978cadac04938b61f2c0d15fafef
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-09 17:31:05 -0300

    Web Scraping

--- seguranca/webScraping.py
+++ seguranca/webScraping.py
@@ -4,10 +4,16 @@ from bs4 import BeautifulSoup
 import requests
 
 # definindo o site a obter informações:
-site = requests.get("https://www.climatempo.com.br/").content
+site = requests.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/558/saopaulo-sp").content
 
 # variavel soup recebe conteúdo de site
 soup = BeautifulSoup(site, 'html.parser')
 
-# transforma html em string e imprime
-print(soup.prettify())
+# # transforma html em string e imprime
+# print(soup.prettify())
+
+#recuperando a temperatura pelo site. Origem: (<p class="-gray _flex _align-center">)
+temperatura = soup.find("span", class_="-gray-light")
+
+# transforma html em string
+print(temperatura.string)
