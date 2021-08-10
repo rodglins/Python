f0beb90954386c51316ce299007519dc784029c7
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-10 11:37:13 -0300

    Web Crawler

--- seguranca/web_crawler.py
+++ seguranca/web_crawler.py
@@ -7,25 +7,32 @@ import operator
 # preenche e manipula estruturas de dados como tuplas, dicionários e listas
 from collections import Counter
 
+# função define o webcrawler
 def start(url):
+    # armazena td o conteudo do site:
     wordlist = []
     source_code = requests.get(url).text
 
+    # faz a requisição dos dados da url e transforma em html
     soup = BeautifulSoup(source_code, 'html.parser')
 
+    # procura td que existe com div e classe:
     for each_text in soup.findAll('div', {'class': 'entry-content'}):
+        # converte em texto (string):
         content = each_text.text
 
+        # transforma em letras minusculas, onde cada conteúdo será uma linha:
         words = content.lower().split()
 
         for each_word in words:
             wordlist.append(each_word)
         clean_wordlist(wordlist)
 
+# função remove qualquer simbolo do wordlist
 def clean_wordlist(wordlist):
     clean_list = []
     for word in wordlist:
-        symbols = '!@#$%¨&*()_+=-[]{}~^,.;:|\/ '
+        symbols = '!@#$%¨&*()_+=-[]{}~^,.;:|\/<>'
 
         for i in range(0, len(symbols)):
             word = word.replace(symbols[i], '')
@@ -34,6 +41,7 @@ def clean_wordlist(wordlist):
             clean_list.append(word)
     create_dictionary(clean_list)
 
+# funcao cria dicionario, fazendo contagem das palavras, evibe, e faz uma lista das mais utilizadas.
 def create_dictionary(clean_list):
     word_count = {}
 
@@ -45,7 +53,7 @@ def create_dictionary(clean_list):
 
     for key, value in sorted(word_count.items(),
                                 key = operator.itemgetter(1)):
-        print('% s : $ s ' % (key, value))
+        print('% s : % s ' % (key, value))
 
     c = Counter(word_count)
 
