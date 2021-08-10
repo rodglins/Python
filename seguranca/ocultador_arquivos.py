ddf3a458d58ca84b71adb68b5f14e48471ae2671
Rodrigo Aglinskas <rodglins@gmail.com>
2021-08-10 12:35:00 -0300

    Ocultador de arquivos

---
+++ seguranca/ocultador_arquivos.py
@@ -0,0 +1,16 @@
+# fornece tipos de dados compatíveis com linguagem C
+# e permite funções de chamada em DLLs ou bibliotecas compartilhadas
+import ctypes
+
+# passando atributos:
+# = hexadecimal
+atributo_ocultar = 0x02
+
+# chamando a biblioteca ctypes a
+# dll para manipulação de arquivo e se torne oculto
+retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+
+if retorno:
+    print('Arquivo foi ocultado')
+else:
+    print('Arquivo não foi ocultado')
